4e03fa192c7fc285f02dc590e6d44c313b8634c1
lee <danishlyy@163.com>
2022-01-27 13:07:28 +0800

    提交代码

---
+++ pro-java/src/main/java/com/pro/java/ProJavaApplication.java
@@ -0,0 +1,13 @@
+package com.pro.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProJavaApplication.class, args);
+    }
+
+}
---
+++ pro-java/src/test/java/com/pro/java/ProJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
